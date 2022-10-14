08a9becbd169b4227a9b4cba757df766c37bae76
Abdelali MAlki <abdelalimalki.ae@gmail.com>
2022-10-14 12:07:01 +0100

    add css

--- prototye_premium/presentation/index.php
+++ prototye_premium/presentation/index.php
@@ -7,15 +7,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0
+    ,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
 <body>
     <div class="cont">
         <div class="input_div">
-            <a href="addpromotion.php">Ajouter promotion</a>
-            <input type="text" id="search" name="search" placeholder="promotion">
+        <form action="../business/addpromotion.php" method="POST">
+        <input type="text" name="nom" placeholder="Ajouter promotion" style="height: 30px; margin-top:50px;margin-left:100px;">
+        <input type="submit" name="add_prom" id="" style=" margin-top:50px;" >
+        <input style="margin-left:600px;height:30px; margin-top:50px;" class="search" type="text" id="search" name="search" placeholder="Chercher promotion"></label>
+
+    </form>
         </div>
-        <table style="width: 100%;">
+        <table  id="customers" style="width: 100%; margin-top:50px;">
             <tr>
                 <th>Nom</th>
                 <th>Action</th>
--- prototye_premium/presentation/updatepromotion.php
+++ prototye_premium/presentation/updatepromotion.php
@@ -7,6 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0
+    ,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
 <body>
@@ -17,9 +26,13 @@
         if ($select_res->num_rows > 0) {
             while ($row = $select_res->fetch_assoc()) {
         ?>
+            <div class="hero">
+                <div style="display:flex;">
                 <input type="text" name="nom" value="<?php echo $row["nom"]; ?>">
                 <input type="hidden" name="id_promotion" value="<?php echo $row["id_promotion"] ?>">
                 <input type="submit" value="Submit" name="update_prom"> 
+                </div>
+            </div>
         <?php
             }
         } 
